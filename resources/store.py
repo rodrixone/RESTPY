b5ea95ac8bc5564273c40994394d9ab2d640ae7d
Rodrix <rodrirodrigezgabriel@gmail.com>
2020-08-21 19:09:25 -0400

    First commit, added REST API

---
+++ resources/store.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource
+from models.store import StoreModel
+
+class Store(Resource):
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'Store not found'},404
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "A store with name '{}' alraedy exists.".format(name)},400
+        store = StoreModel(name)
+        store.save_to_db()
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+        return {'message': 'Store deleted'}
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
---
+++ security.py
@@ -0,0 +1,10 @@
+from models.user import UserModel
+from werkzeug.security import safe_str_cmp
+import sqlite3 as sq
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if(user and safe_str_cmp(user.password, password)):
+        return user
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
